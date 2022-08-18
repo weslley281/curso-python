c86b78df8d4d33b5899d3044700442c2a05088aa
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-18 09:55:21 -0400

    mais operadores

---
+++ maisOperadores.py
@@ -0,0 +1,23 @@
+from re import X
+
+
+lista = [ 1, 2, 3, 'Ana', 'Carla']
+
+2 in lista
+print('Ana' not in lista)
+
+x = 3
+y = x
+z = 3
+
+print(x is y)
+print(y is z)
+print(x is not z)
+
+lista_a = [1, 2, 3]
+lista_b = lista_a
+lista_c = [1, 2, 3]
+
+print(lista_a is lista_b)
+print(lista_b is lista_c)
+print(lista_a is not lista_c)
