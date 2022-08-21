71ff0cba31be9672972e02df365eca52a446b09b
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-21 12:08:16 -0400

    tipos numericos

---
+++ Tipos Numericos.py
@@ -0,0 +1,14 @@
+a = 5
+b = 5.5
+print(type(a + b))
+print(type(a / b))
+print(type(a * b))
+print(type(a - b))
+
+print(b.is_integer)
+print(int.__add__(2, 3))
+print((-2).__abs__())
+print(type(-2))
+
+print((-3.5).__abs__())
+
