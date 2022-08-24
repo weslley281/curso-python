e26e8d4646cc9d104ca06ac0e87fe9e376f6533c
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-08-24 09:30:12 -0400

    string

---
+++ fundamental/tipoString.py
@@ -0,0 +1,17 @@
+nome = "Weslley Ferraz"
+
+print(nome[3])
+
+print(nome[6], nome[5], nome[4])
+
+print(nome[:7])
+
+print('Wes' in nome)
+
+print(len(nome))
+
+print(nome.upper())
+
+print(nome.lower())
+
+print(nome.split(" "))
