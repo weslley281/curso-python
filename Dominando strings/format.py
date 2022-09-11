f03a25527736523251d51b5f2e7eae8a8351d563
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-11 08:56:57 -0400

    format

---
+++ Dominando strings/format.py
@@ -0,0 +1,11 @@
+# interpolação
+nome = "Weslley"
+sobrenome = "Ferraz"
+idade = 26
+
+print("Meu nome é {nome} {sobrenome} e a minha idade é {idade}".format(
+    nome=nome, sobrenome=sobrenome, idade=idade))
+
+# reduz as casas decimais
+valor = 15.4678
+print("O valor é R$ {valor:.2f}".format(valor=valor))
