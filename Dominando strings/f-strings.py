4bea46bb8f4c7368e29e8aa44591ac2b934a1b93
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-11 12:59:47 -0400

    f-string

---
+++ Dominando strings/f-strings.py
@@ -0,0 +1,11 @@
+titulo = "Monitor Gamer"
+polegadas = 27
+preco = 299.99
+
+print(f'Nome do produto: {titulo} e o tamanho é {polegadas} polegadas')
+
+print(
+    f'Nome do produto: {titulo.upper():*^50}\n'
+    f'e o tamanho é {polegadas} polegadas\n'
+    f'e o preço é {preco}'
+)
