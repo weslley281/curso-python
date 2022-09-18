f2ab67fd9fbcb727215e409f53223bba33a18e21
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-18 10:49:00 -0400

    conversção de tipos numéricos

---
+++ Dominando tipos numericos/conversaoDeTipos.py
@@ -0,0 +1,15 @@
+idade = 35
+texto = "Parabens Fulano pelo seus " + str(idade) + ' anos de idade'
+
+print(texto)
+
+pontuacao_srt = '10'
+pontuacao_int = int(pontuacao_srt)
+
+print(pontuacao_int)
+print(f'tipo: {type(pontuacao_int)}')
+
+pontuacao_float = float(pontuacao_int)
+
+print(pontuacao_float)
+print(f'tipo: {type(pontuacao_float)}')
