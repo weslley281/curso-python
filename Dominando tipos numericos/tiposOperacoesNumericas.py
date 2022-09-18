0e6222aed3bbd7137cfc5b851c904d9258c4ab41
Weslley Ferraz <weslleyhenrique800@gmail.com>
2022-09-17 21:00:37 -0400

    tipos de operações com numeros

---
+++ Dominando tipos numericos/tiposOperacoesNumericas.py
@@ -0,0 +1,10 @@
+numero_1 = 21
+numero_2 = 100
+numero_3 = 0.5
+
+print(f'a soma de {numero_1} + {numero_2} = {numero_1 + numero_2}')
+print(f'a subtração de {numero_1} - {numero_2} = {numero_1 - numero_2}')
+print(f'a multiplicação de {numero_1} * {numero_2} = {numero_1 * numero_2}')
+print(f'a divisão de {numero_1} / {numero_2} = {numero_1 / numero_2}')
+print(f'o resto de {numero_1} e {numero_2} = {numero_1 % numero_2}')
+print(f'a potencia {numero_1} + {numero_2} = {numero_1 ** numero_2}')
