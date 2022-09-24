80ea03f93c5026faa45477ffe1c733851e7df9a1
Weslley Henrique Vieira Ferraz <65488214+weslley281@users.noreply.github.com>
2022-09-24 12:56:14 -0300

    creating input

---
+++ entrada de usuários/funcaoInput.py
@@ -0,0 +1,12 @@
+nome = input("Qual é o seu nome: ")
+print(f"Seu nome é {nome}")
+
+numero1 = input("Digite o primeiro numero: ")
+numero2 = input("Digite o primeiro numero: ")
+
+numero1fotmatado = int(numero1)
+numero2fotmatado = int(numero2)
+
+resultado = numero1fotmatado + numero2fotmatado
+
+print(f"O resultado da soma desses numeros {nome} é {resultado}")
