938ee2b11825b218891d1c0f4835b0246e8e3f48
Weslley Henrique Vieira Ferraz <65488214+weslley281@users.noreply.github.com>
2022-09-28 09:46:30 -0300

    a

---
+++ Dominando Dicionários/adcionandoElementos.py
@@ -0,0 +1,10 @@
+computador = {
+    'cpu': 'core i7',
+    'ram': 'DD4 16GB',
+    'ssd': 'Sansung 1TB',
+    'gpu': 'RTX 2080ti',
+}
+
+print(f"Computador v1: {computador}")
+
+computador['ram'] = 'DDR5 32Gb'
