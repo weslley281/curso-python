6fb07cb1c6fa0c19581f0cb810957b0ad93c63b9
Weslley Henrique Vieira Ferraz <65488214+weslley281@users.noreply.github.com>
2022-09-29 12:28:24 -0300

    update de dicionários

--- Dominando Dicionários/adcionandoElementos.py
+++ Dominando Dicionários/adcionandoElementos.py
@@ -1,3 +1,6 @@
+from tkinter import font
+
+
 computador = {
     'cpu': 'core i7',
     'ram': 'DD4 16GB',
@@ -8,3 +11,13 @@ computador = {
 print(f"Computador v1: {computador}")
 
 computador['ram'] = 'DDR5 32Gb'
+
+print(f'Computador v2: {computador}')
+
+computador['fonte'] = 'Fonte 600W'
+
+print(f'Computador v3: {computador}')
+
+computador.update({'fonte': 'Fonte 800W', 'gpu': 'GTX 710'})
+
+print(f'Computador v4: {computador}')
